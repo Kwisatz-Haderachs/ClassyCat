6a656397e3f93d8ddb99c7fa3bd7496c3c377b35
kwisatz-haderachs <frcranmer@gmail.com>
2022-08-11 16:17:33 -0500

    intial init, no functional code

---
+++ src/main/java/classyCat/BankAccount.java
@@ -0,0 +1,26 @@
+package classyCat;
+
+public class BankAccount {
+    private double balance;
+
+}
+//    long cents;
+//    long balance;
+//
+//    long getBalance(){
+//        return balance;
+//    }
+//
+//    void withdraw(long cents){
+//        this.balance -= cents;
+//    }
+//    void deposit(long cents){
+//        this.balance += cents;
+//    }
+//
+//    boolean isOverDrawn(){
+//        if (balance < 0.0){
+//            return true;
+//        }
+//        return false;
+//    }
